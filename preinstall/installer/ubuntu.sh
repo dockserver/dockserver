1ed076f2b3a4c7884561418391cbe742678858de
DrAg0n141 <44865095+DrAg0n141@users.noreply.github.com>
2021-09-22 13:30:51 +0200

    Update ubuntu.sh

--- preinstall/installer/ubuntu.sh
+++ preinstall/installer/ubuntu.sh
@@ -125,7 +125,7 @@ EOF
   if [[ -x $(command -v lshw) ]];then
      gpu="ntel NVIDIA"
      for i in ${gpu};do
-         TDV=$(lspci | grep -i --color 'vga\|3d\|2d' | grep -E $i 1>/dev/null 2>&1 && echo true || echo false)
+         TDV=$(lspci | grep -i --color 'vga\|display\|3d\|2d' | grep -E $i 1>/dev/null 2>&1 && echo true || echo false)
          if [[ $TDV == "true" ]];then $(command -v bash) ${source}/gpu.sh;fi
      done
   fi
