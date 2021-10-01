ee2cf4625561cb44461dc9ea033eac011be35a8c
doob187 <60312740+doob187@users.noreply.github.com>
2021-10-01 21:21:46 +0200

    Update ubuntu.sh
    Use v2

--- preinstall/installer/ubuntu.sh
+++ preinstall/installer/ubuntu.sh
@@ -109,12 +109,7 @@ EOF
   if [[ $networkcheck == "false" ]];then $(command -v docker) network create --driver=bridge socket-proxy 1>/dev/null 2>&1;fi  
   if [[ ! -x $(command -v rsync) ]];then $(command -v apt) install --reinstall rsync -yqq 1>/dev/null 2>&1;fi
   if [ ! -x $(command -v docker-compose) ] || [ -x $(command -v docker-compose) ];then
-     COMPOSE_VERSION=$($(command -v curl) --silent -fsSL https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
-     sh -c "curl --silent -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-     sh -c "curl --silent -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
-     if [[ ! -L "/usr/bin/docker-compose" ]];then $(command -v rm) -f /usr/bin/docker-compose && ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose;fi
-     $(command -v chmod) a=rx,u+w /usr/local/bin/docker-compose >/dev/null 2>&1
-     $(command -v chmod) a=rx,u+w /usr/bin/docker-compose >/dev/null 2>&1
+     curl -fL https://raw.githubusercontent.com/docker/compose-cli/main/scripts/install/install_linux.sh | sh
   fi
      dailyapt=$($(command -v systemctl) is-active apt-daily | grep -qE 'active' && echo true || echo false)
      dailyupg=$($(command -v systemctl) is-active apt-daily-upgrade | grep -qE 'active' && echo true || echo false)
