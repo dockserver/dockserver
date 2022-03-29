70557853f38b7ce7bbb38ca21a4bd6eb559f23a6
doob187 <60312740+doob187@users.noreply.github.com>
2022-03-29 19:34:44 +0200

    Update wgetfile.sh

--- wgetfile.sh
+++ wgetfile.sh
@@ -20,7 +20,7 @@ function log() {
 
 function rmdocker() {
 if [ $(which docker) ]; then
-   dockers=$(docker ps -aq --format '{{.Names}}' | sed '/^$/d' | grep -E 'dockserver')
+   dockers=$(docker ps -aq --format '{{.Names}}' | sed '/^$/d' | grep 'dockserver')
    $(which docker) stop $dockers > /dev/null
    $(which docker) rm $dockers > /dev/null
    $(which docker) system prune -af > /dev/null
@@ -30,73 +30,76 @@ fi
 
 function pulldockserver() {
 if [ $(which docker) ]; then
-   $(which docker) pull -q docker.dockserver.io/dockserver/docker-dockserver && \
+   $(which docker) pull -q docker.dockserver.io/dockserver/docker-dockserver
    $(which docker) run -d \
-  --name=dockserver \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=Europe/London \
-  -v /opt/dockserver:/opt/dockserver:rw \
-  docker.dockserver.io/dockserver/docker-dockserver
+   --name=dockserver \
+   -e PUID=1000 -e PGID=1000 -e TZ=Europe/London \
+   -v /opt/dockserver:/opt/dockserver:rw \
+   docker.dockserver.io/dockserver/docker-dockserver
+   $(which chown) -R 1000:1000 /opt/dockserver
 fi
 }
 
+function upsys() {
 updates="update upgrade autoremove autoclean"
 for upp in ${updates}; do
+    log "**** update system ****" && \
     sudo $(command -v apt) $upp -yqq 1>/dev/null 2>&1 && clear
 done
 unset updates
 
-packages=(curl bc tar git jq pv pigz tzdata rsync)
-log "**** install build packages ****" && \
-sudo $(command -v apt) install $packages -yqq 1>/dev/null 2>&1 && clear
+packages="curl bc sudo wget tar git jq pv pigz tzdata rsync"
+for pack in ${packages}; do
+    log "**** install build packages ****" && \
+    sudo $(command -v apt) $pack -yqq 1>/dev/null 2>&1
+done
 unset packages
 
-remove=(/bin/dockserver /usr/bin/dockserver)
-log "**** remove old dockserver bins ****" && \
-sudo $(command -v rm) -rf $remove 1>/dev/null 2>&1 && clear
+remove="/bin/dockserver /usr/bin/dockserver"
+for rmold in ${packages}; do
+    log "**** remove old links  ****" && \
+    sudo $(command -v rm) -rf $rmold 1>/dev/null 2>&1
+done
 unset remove
 
+if [ $(which snapd) ]; then
+   if [[ -d "/var/cache/snapd/" ]];then
+      sudo $(which rm) -rf /var/cache/snapd/
+   fi
+   sudo $(which apt) autoremove --purge snapd gnome-software-plugin-snap -yqq && \
+   sudo $(which apt) autoclean -yqq
+fi
+}
+
+function dockcomp() {
 if [ ! $(which docker) ]; then
-   $(which curl) -fsSL https://get.docker.com -o /tmp/docker.sh && bash /tmp/docker.sh
+   log "**** installing now docker ****" && \
+   $(which wget) -qO- https://get.docker.com/ | sh >/dev/null 2>&1 && \
    $(which systemctl) reload-or-restart docker.service
 fi
 
 if [ ! $(which docker-compose) ]; then
-    $(which curl) -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
-    $(which ln) -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
-    $(which chmod) +x /usr/local/bin/docker-compose /usr/bin/docker-compose
+   log "**** installing now docker-composer ****" && \
+   $(which curl) -fsSL --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
+   $(which ln) -sf /usr/local/bin/docker-compose /usr/bin/docker-compose && \
+   $(which chmod) +x /usr/local/bin/docker-compose /usr/bin/docker-compose
 fi
+}
 
-[[ ! -d "/opt/dockserver" ]] && mkdir -p /opt/dockserver
-
-file=/opt/dockserver/.installer/dockserver
-store=/usr/bin/dockserver
-dockserver=/opt/dockserver
-
-while true; do
-if [ "$(ls -A $dockserver)" ]; then
-   rmdocker && sleep 3 && break
-else
-   pulldockserver
-   echo "dockserver is not pulled yet" 
-   sleep 5 && continue
-fi
-done
-
+function finalend() {
 if test -f "/usr/bin/dockserver"; then $(which rm) /usr/bin/dockserver ; fi
 if [[ $EUID != 0 ]]; then
     $(which chown) -R $(whoami):$(whoami) /opt/dockserver
     $(which usermod) -aG sudo $(whoami)
-    cp /opt/dockserver/.installer/dockserver /usr/bin/dockserver
-    ln -sf /usr/bin/dockserver /bin/dockserver
-    chmod +x /usr/bin/dockserver
+    $(which cp) /opt/dockserver/.installer/dockserver /usr/bin/dockserver
+    $(which ln) -sf /usr/bin/dockserver /bin/dockserver
+    $(which chmod) +x /usr/bin/dockserver
     $(which chown) $(whoami):$(whoami) /usr/bin/dockserver 
 else 
     $(which chown) -R 1000:1000 /opt/dockserver
-    cp /opt/dockserver/.installer/dockserver /usr/bin/dockserver
-    ln -sf /usr/bin/dockserver /bin/dockserver
-    chmod +x /usr/bin/dockserver
+    $(which cp) /opt/dockserver/.installer/dockserver /usr/bin/dockserver
+    $(which ln) -sf /usr/bin/dockserver /bin/dockserver
+    $(which chmod) +x /usr/bin/dockserver
     $(which chown) -R 1000:1000 /usr/bin/dockserver
 fi
 
@@ -113,4 +116,28 @@ printf "
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 " 
+}
+
+#### run in order
+
+upsys && dockcomp
+
+[[ ! -d "/opt/dockserver" ]] && mkdir -p /opt/dockserver
+
+file=/opt/dockserver/.installer/dockserver
+store=/usr/bin/dockserver
+dockserver=/opt/dockserver
+
+while true; do
+   if [ "$(ls -A $dockserver)" ]; then
+      rmdocker && sleep 3 && break
+   else
+      pulldockserver
+      echo "dockserver is not pulled yet" 
+      sleep 5 && continue
+   fi
+done
+
+finalend
+
 #EOF#
