b0f913fe7b80a4dcf1ab7112f98d0ed79e3ec0d2
doob187 <60312740+doob187@users.noreply.github.com>
2022-07-25 19:09:41 +0200

    Update wgetfile.sh

--- wgetfile.sh
+++ wgetfile.sh
@@ -15,28 +15,30 @@
 # shellcheck disable=SC2046
 
 function log() {
-   echo "[INSTALL] DockServer ${1}"
+   $SUID echo "[INSTALL] DockServer ${1}"
 }
 
 function rmdocker() {
 if [ $(which docker) ]; then
    dockers=$(docker ps -aq --format '{{.Names}}' | sed '/^$/d' | grep 'dockserver')
-   $(which docker) stop $dockers > /dev/null
-   $(which docker) rm $dockers > /dev/null
-   $(which docker) system prune -af > /dev/null
+   if [[ $dockers != "" ]]; then
+      $SUID $(which docker) stop $dockers > /dev/null
+      $SUID $(which docker) rm $dockers > /dev/null
+      $SUID $(which docker) system prune -af > /dev/null
+   fi
    unset $dockers
 fi
 }
 
 function pulldockserver() {
 if [ $(which docker) ]; then
-   $(which docker) pull -q docker.dockserver.io/dockserver/docker-dockserver
-   $(which docker) run -d \
+   $SUID $(which docker) pull -q docker.dockserver.io/dockserver/docker-dockserver
+   $SUID $(which docker) run -d \
    --name=dockserver \
    -e PUID=1000 -e PGID=1000 -e TZ=Europe/London \
    -v /opt/dockserver:/opt/dockserver:rw \
    docker.dockserver.io/dockserver/docker-dockserver
-   $(which chown) -R 1000:1000 /opt/dockserver
+   $SUID $(which chown) -R 1000:1000 /opt/dockserver
 fi
 }
 
@@ -44,51 +46,68 @@ function upsys() {
 log "**** update system ****"
 updates="update upgrade autoremove autoclean"
 for upp in ${updates}; do
-    sudo $(command -v apt) $upp -yqq 1>/dev/null 2>&1 && clear
+    $SUID $(which apt) $upp -yqq 1>/dev/null 2>&1 && clear
 done
 unset updates
 
 log "**** install build packages ****"
 packages="curl bc sudo wget tar git jq pv pigz tzdata rsync"
 for pack in ${packages}; do
-    sudo $(command -v apt) $pack -yqq 1>/dev/null 2>&1
+    $SUID $(which apt) $pack -yqq 1>/dev/null 2>&1
 done
 unset packages
 
 log "**** remove old links  ****"
 remove="/bin/dockserver /usr/bin/dockserver"
 for rmold in ${remove}; do
-    sudo $(command -v rm) -rf $rmold 1>/dev/null 2>&1
+    $SUID $(which rm) -rf $rmold 1>/dev/null 2>&1
 done
 unset remove
 
 if [ $(which snapd) ]; then
    log "**** snapd  ****" && \
    if [[ -d "/var/cache/snapd/" ]];then
-      sudo $(which rm) -rf /var/cache/snapd/
+      $SUID $(which rm) -rf /var/cache/snapd/
    fi
-   sudo $(which apt) autoremove --purge snapd gnome-software-plugin-snap -yqq && \
-   sudo $(which apt) autoclean -yqq
+   $SUID $(which apt) autoremove --purge snapd gnome-software-plugin-snap -yqq && \
+   $SUID $(which apt) autoclean -yqq
 fi
 }
 
 function dockcomp() {
 if [ ! $(which docker) ]; then
    log "**** installing now docker ****" && \
-   $(which wget) -qO- https://get.docker.com/ | sh >/dev/null 2>&1 && \
-   $(which systemctl) reload-or-restart docker.service
+   $SUID $(which wget) -qO- https://get.docker.com/ | sh >/dev/null 2>&1 && \
+   $SUID $(which systemctl) reload-or-restart docker.service
 fi
 
 if [ ! $(which docker-compose) ]; then
-   log "**** installing now docker-composer ****" && \
-   $(which curl) -fsSL --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
-   $(which ln) -sf /usr/local/bin/docker-compose /usr/bin/docker-compose && \
-   $(which chmod) +x /usr/local/bin/docker-compose /usr/bin/docker-compose
-fi
+   export DOCKER_CONFIG=${DOCKER_CONFIG:-/opt/appdata/.docker}
+   VERSION="$($(which curl) -sX GET https://api.github.com/repos/docker/compose/releases/latest | jq --raw-output '.tag_name')"
+   ARCH=$(uname -m)
+   log "installing now docker composer $VERSION on $ARCH ...." && \
+   $SUID $(which mkdir) $DOCKER_CONFIG/cli-plugins && \
+   $SUID $(which curl) -fsSL "https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-linux-${ARCH}" -o $DOCKER_CONFIG/cli-plugins/docker-compose
+   if test -f $DOCKER_CONFIG/cli-plugins/docker-compose;then
+      $SUID $(which chmod) +x $DOCKER_CONFIG/cli-plugins/docker-compose && \
+      $SUID $(which rm) -f /usr/bin/docker-compose /usr/local/bin/docker-compose && \
+      $SUID $(which cp) -r $DOCKER_CONFIG/cli-plugins/docker-compose /usr/bin/docker-compose && \
+      $SUID $(which cp) -r $DOCKER_CONFIG/cli-plugins/docker-compose /usr/local/bin/docker-compose
+  else
+      sleep 5 ## wait time before next pull
+      $SUID $(which mkdir) -p $DOCKER_CONFIG/cli-plugins && \
+      $SUID $(which curl) -fsSL https://github.com/docker/compose/releases/download/$VERSION/docker-compose-linux-`$(uname -m)` -o $DOCKER_CONFIG/cli-plugins/docker-compose && \
+      $SUID $(which rm) -f /usr/bin/docker-compose /usr/local/bin/docker-compose && \
+      $SUID $(which chmod) +x $DOCKER_CONFIG/cli-plugins/docker-compose && \
+      $SUID $(which cp) -r $DOCKER_CONFIG/cli-plugins/docker-compose /usr/bin/docker-compose && \
+      $SUID $(which cp) -r $DOCKER_CONFIG/cli-plugins/docker-compose /usr/local/bin/docker-compose
+  fi
 }
 
 function finalend() {
-if test -f "/usr/bin/dockserver"; then $(which rm) /usr/bin/dockserver ; fi
+if test -f "/usr/bin/dockserver"; then
+    $(which rm) /usr/bin/dockserver /bin/dockserver
+fi
 if [[ $EUID != 0 ]]; then
     $(which chown) -R $(whoami):$(whoami) /opt/dockserver
     $(which usermod) -aG sudo $(whoami)
@@ -114,6 +133,12 @@ printf "
      [ sudo ] dockserver -h / --help
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 "
+}
+
+function SUID() {
+if [[ $EUID != 0 ]]; then
+   sudo "$1"
+fi
 }
 
 #### run in order
