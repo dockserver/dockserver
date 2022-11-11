cc17d610420c4d42815d5d47d262f3985b924834
DrAg0n141 <44865095+drag0n141@users.noreply.github.com>
2022-11-11 10:09:33 +0100

    Upload Files

---
+++ backup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/with-contenv bash
+# shellcheck shell=bash
+##################################
+# Copyright (c) 2021,  : MrDoob  #
+# Docker owner         : doob187 #
+# Docker Maintainer    : doob187 #
+# Code owner           : doob187 #
+#     All rights reserved        #
+##################################
+# THIS DOCKER IS UNDER LICENSE   #
+# NO CUSTOMIZING IS ALLOWED      #
+# NO REBRANDING IS ALLOWED       #
+# NO CODE MIRRORING IS ALLOWED   #
+##################################
+# shellcheck disable=SC2086
+# shellcheck disable=SC2006
+
+$(command -v docker) system prune -af 1>/dev/null 2>&1
+$(command -v docker) pull ghcr.io/dockserver/docker-backup:latest 1>/dev/null 2>&1
+dock=$(docker ps -aq --format '{{.Names}}' | grep -v 'trae' | grep -v 'auth' | grep -v 'cf')
+for i in ${dock}; do
+    $(command -v docker) run --rm -v /opt/appdata:/backup/$i -v /mnt:/mnt ghcr.io/dockserver/docker-backup:latest backup $i local
+    $(command -v chown) -cR 1000:1000 /mnt/downloads/appbackups/local/$i.tar.gz 1>/dev/null 2>&1
+done
+$(command -v docker) system prune -af 1>/dev/null 2>&1
+exit
+#EOF
---
+++ wgetfile.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+####################################
+# All rights reserved.              #
+# started from Zero                 #
+# Docker owned dockserver           #
+# Docker Maintainer dockserver      #
+#####################################
+#####################################
+# THIS DOCKER IS UNDER LICENSE      #
+# NO CUSTOMIZING IS ALLOWED         #
+# NO REBRANDING IS ALLOWED          #
+# NO CODE MIRRORING IS ALLOWED      #
+#####################################
+# shellcheck disable=SC2086
+# shellcheck disable=SC2046
+
+function log() {
+   echo "[INSTALL] DockServer ${1}"
+}
+
+function rmdocker() {
+if [ $(which docker) ]; then
+   dockers=$(docker ps -aq --format '{{.Names}}' | sed '/^$/d' | grep -E 'dockserver')
+   $(which docker) stop $dockers > /dev/null
+   $(which docker) rm $dockers > /dev/null
+   $(which docker) system prune -af > /dev/null
+   unset $dockers
+fi
+}
+
+function pulldockserver() {
+if [ $(which docker) ]; then
+   $(which docker) pull -q docker.dockserver.io/dockserver/docker-dockserver && \
+   $(which docker) run -d \
+  --name=dockserver \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Europe/London \
+  -v /opt/dockserver:/opt/dockserver:rw \
+  docker.dockserver.io/dockserver/docker-dockserver
+fi
+}
+
+updates="update upgrade autoremove autoclean"
+for upp in ${updates}; do
+    sudo $(command -v apt) $upp -yqq 1>/dev/null 2>&1 && clear
+done
+unset updates
+
+packages=(curl bc tar git jq pv pigz tzdata rsync)
+log "**** install build packages ****" && \
+sudo $(command -v apt) install $packages -yqq 1>/dev/null 2>&1 && clear
+unset packages
+
+remove=(/bin/dockserver /usr/bin/dockserver)
+log "**** remove old dockserver bins ****" && \
+sudo $(command -v rm) -rf $remove 1>/dev/null 2>&1 && clear
+unset remove
+
+if [ ! $(which docker) ]; then
+   $(which curl) -fsSL https://get.docker.com -o /tmp/docker.sh && bash /tmp/docker.sh
+   $(which systemctl) reload-or-restart docker.service
+fi
+
+if [ ! $(which docker-compose) ]; then
+    $(which curl) -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
+    $(which ln) -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
+    $(which chmod) +x /usr/local/bin/docker-compose /usr/bin/docker-compose
+fi
+
+[[ ! -d "/opt/dockserver" ]] && mkdir -p /opt/dockserver
+
+file=/opt/dockserver/.installer/dockserver
+store=/usr/bin/dockserver
+dockserver=/opt/dockserver
+
+while true; do
+if [ "$(ls -A $dockserver)" ]; then
+   rmdocker && sleep 3 && break
+else
+   pulldockserver
+   echo "dockserver is not pulled yet" 
+   sleep 5 && continue
+fi
+done
+
+if test -f "/usr/bin/dockserver"; then $(which rm) /usr/bin/dockserver ; fi
+if [[ $EUID != 0 ]]; then
+    $(which chown) -R $(whoami):$(whoami) /opt/dockserver
+    $(which usermod) -aG sudo $(whoami)
+    cp /opt/dockserver/.installer/dockserver /usr/bin/dockserver
+    ln -sf /usr/bin/dockserver /bin/dockserver
+    chmod +x /usr/bin/dockserver
+    $(which chown) $(whoami):$(whoami) /usr/bin/dockserver 
+else 
+    $(which chown) -R 1000:1000 /opt/dockserver
+    cp /opt/dockserver/.installer/dockserver /usr/bin/dockserver
+    ln -sf /usr/bin/dockserver /bin/dockserver
+    chmod +x /usr/bin/dockserver
+    $(which chown) -R 1000:1000 /usr/bin/dockserver
+fi
+
+printf "
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+    🚀    DockServer
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+     install dockserver
+     [ sudo ] dockserver -i
+
+     all commands
+     [ sudo ] dockserver -h / --help
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+" 
+#EOF#
