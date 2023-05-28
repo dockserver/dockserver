b9e642887727c3a7eff471d60034e83a893d79e6
DrAg0n141 <44865095+drag0n141@users.noreply.github.com>
2023-05-28 12:34:26 +0200

    Update gpu.sh
    Signed-off-by: DrAg0n141 <44865095+drag0n141@users.noreply.github.com>

--- preinstall/templates/local/gpu.sh
+++ preinstall/templates/local/gpu.sh
@@ -69,9 +69,10 @@ nvidiagpu() {
     ## bypass to check supported OS
     subos
     if [[ $RCHK == "false" ]]; then
-        curl -fsSL https://nvidia.github.io/nvidia-docker/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-docker-archive-keyring.gpg
-        curl -s -L https://nvidia.github.io/nvidia-docker/$DIST/nvidia-docker.list | \
-        tee /etc/apt/sources.list.d/nvidia-docker.list
+        curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+        curl -s -L https://nvidia.github.io/libnvidia-container/$DIST/libnvidia-container.list | \
+        sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+        sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
     fi
     if [[ $CHKN != "true" ]]; then
         package_list="nvidia-container-toolkit nvidia-container-runtime"
